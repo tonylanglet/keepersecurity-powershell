2ff972062cb79ff3267d61d0a10c8049d65c70a9
tonylanglet <tony.langlet@core-portal.com>
2019-02-25 11:25:40 +0100

    Create Get-KeeperRecordAttachment.py

---
+++ PyScripts/Get-KeeperRecordAttachment.py
@@ -0,0 +1,57 @@
+import sys
+import getopt
+import getpass
+import string
+import re
+import argparse
+
+from keepercommander.record import Record
+from keepercommander.commands.record import RecordDownloadAttachmentCommand
+from keepercommander.params import KeeperParams
+from keepercommander import display, api
+
+my_params = KeeperParams()
+
+# KEEPER COMMAND
+def get_recordatt(recordUID):
+    command = RecordDownloadAttachmentCommand()
+    result = command.execute(my_params, record=recordUID)
+    print("Successfully")
+    return result
+      
+# MAIN FUNCTION
+def main(argv):
+    # Record parameters
+    recordUID = None
+    # Authentication credentials
+    authUsername = None
+    authPassword = None
+
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", --record", type=str, help="Folder UID", required=True)
+    parser.add_argument("-auser", "--ausername", type=str, help="Authentication username", required=True)
+    parser.add_argument("-apass", "--apassword", type=str, help="Authentication password", required=True)
+    args = parser.parse_args()
+
+    if args.folder:
+        folderUID = args.folder
+    if args.force:
+        folderForce = args.force
+    if args.ausername:
+        authUsername = args.ausername
+    if args.apassword:
+        authPassword = args.apassword
+    
+    while not my_params.user:
+        my_params.user = authUsername
+
+    while not my_params.password:
+        my_params.password = authPassword
+    api.sync_down(my_params)
+
+    # Start function
+    result = get_recordatt(recordUID)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
