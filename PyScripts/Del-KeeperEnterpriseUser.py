ce33f8bc3e48c10c46aec327bf4edf704d0bbe5f
tonylanglet <tony.langlet@core-portal.com>
2019-02-26 20:06:14 +0100

    Create Del-KeeperEnterpriseUser.py

---
+++ PyScripts/Del-KeeperEnterpriseUser.py
@@ -0,0 +1,54 @@
+import sys
+import getopt
+import getpass
+import string
+import argparse
+
+from keepercommander.record import Record
+from keepercommander.commands.register import EnterpriseUserCommand
+from keepercommander.params import KeeperParams
+from keepercommander import display, api
+
+my_params = KeeperParams()
+      
+# MAIN FUNCTION
+def main(argv):
+    # Variables
+    entUserEmail = None
+    entUserDelete = True
+    entUserForce = True
+    # Authentication credentials
+    authUsername = None
+    authPassword = None
+
+    #Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--email', type=str, help='user email or user ID or user search pattern')
+    parser.add_argument('-d', '--delete', type=str, help='delete user')
+    parser.add_argument('-auser', '--ausername', type=str, help='Authentication username', required=True)
+    parser.add_argument('-apass', '--apassword', type=str, help='Authentication password', required=True)
+    args = parser.parse_args()
+
+    if args.email:
+        entUserEmail = args.email
+    if args.ausername:
+        authUsername = args.ausername
+    if args.apassword:
+        authPassword = args.apassword
+
+    #Authentication login
+    while not my_params.user:
+        my_params.user = authUsername
+
+    while not my_params.password:
+        my_params.password = authPassword
+    api.sync_down(my_params)
+
+    # KEEPER COMMAND
+    command = EnterpriseUserCommand()
+    result = command.execute(my_params, email=entUserEmail, delete=entUserDelete, force=entUserForce)
+    print("Successfully")
+    return result
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
