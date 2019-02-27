04685de763bd6756c4d3f88a296e0a758968049b
tonylanglet <tony.langlet@core-portal.com>
2019-02-27 23:29:45 +0100

    Update Set-KeeperEnterpriseTeam.py

--- PyScripts/Set-KeeperEnterpriseTeam.py
+++ PyScripts/Set-KeeperEnterpriseTeam.py
@@ -19,31 +19,33 @@ def main(argv):
 
     #Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--team', type=str, help='add user to role')
+    parser.add_argument('-t', '--team', type=str, help='team id')
     parser.add_argument('-au', '--add-user', type=str, help='add user to team')
-    parser.add_argument('-re', '--restrict-edit', type=str, help='remove user from role')
-    parser.add_argument('-rs', '--restrict-share', type=str, help='remove user from role')
-    parser.add_argument('-rv', '--restrict-view', type=str, help='remove user from role')
-    parser.add_argument('-no', '--node', type=str, help='remove user from role')
+    parser.add_argument('-ru', '--remove-user', type=str, help='remove user to team')
+    parser.add_argument('-re', '--restrict-edit', type=str, help='restrict edit')
+    parser.add_argument('-rs', '--restrict-share', type=str, help='restrict share')
+    parser.add_argument('-rv', '--restrict-view', type=str, help='restrict view')
+    parser.add_argument('-no', '--node', type=str, help='node name or uid')
     parser.add_argument('-auser', '--ausername', type=str, help='Authentication username', required=True)
     parser.add_argument('-apass', '--apassword', type=str, help='Authentication password', required=True)
     args = parser.parse_args()
 
     Parameters = dict()
     if args.team is not None:
-        Parameters.Update('team',args.team)
+        Parameters.Update({'team':args.team})
     if args.add-user is not None:
-        Parameters.Update('add-user',args.add-user)
+        Parameters.Update({'add-user':args.add-user})
     if args.remove-user is not None:
-        Parameters.Update('remove-user',args.remove-user)
+        Parameters.Update({'remove-user':args.remove-user})
     if args.restrict-edit is not None:
-        Parameters.Update('restrict-edit',args.restrict-edit)
+        Parameters.Update({'restrict-edit':args.restrict-edit})
     if args.restrict-share is not None:
-        Parameters.Update('restrict-share',args.restrict-share)
+        Parameters.Update({'restrict-share':args.restrict-share})
     if args.restrict-view is not None:
-        Parameters.Update('restrict-view',args.restrict-view)
+        Parameters.Update({'restrict-view':args.restrict-view})
     if args.node is not None:
-        Parameters.Update('noder',args.node)
+        Parameters.Update({'node':args.node})
+            
     if args.ausername:
         authUsername = args.ausername
     if args.apassword:
