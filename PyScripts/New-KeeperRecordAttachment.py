2fcc6d3527bad2f0fae638833b6621e61f90b9bc
tonylanglet <tony.langlet@core-portal.com>
2019-02-28 00:00:24 +0100

    Update New-KeeperRecordAttachment.py

--- PyScripts/New-KeeperRecordAttachment.py
+++ PyScripts/New-KeeperRecordAttachment.py
@@ -2,7 +2,6 @@ import sys
 import getopt
 import getpass
 import string
-import re
 import argparse
 
 from keepercommander.record import Record
@@ -14,26 +13,24 @@ my_params = KeeperParams()
       
 # MAIN FUNCTION
 def main(argv):
-    # Variables
-    recordUID = None
-    recordFile = None
-    attName = None
     # Authentication credentials
     authUsername = None
     authPassword = None
 
     # Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--record", type=str, help="Folder UID", required=True)
-    parser.add_argument("-f", "--file", type=str, help="force y/n, true/false", default=True)
-    parser.add_argument("-auser", "--ausername", type=str, help="Authentication username", required=True)
-    parser.add_argument("-apass", "--apassword", type=str, help="Authentication password", required=True)
+    parser.add_argument('-r', '--record', type=str, help='Folder UID', required=True)
+    parser.add_argument('-f', '--file', type=str, help='file path', required=True)
+    parser.add_argument('-auser', '--ausername', type=str, help='Authentication username', required=True)
+    parser.add_argument('-apass', '--apassword', type=str, help='Authentication password', required=True)
     args = parser.parse_args()
 
-    if args.record:
-        recordUID = args.record
-    if args.file:
-        recordFile = args.file
+    Parameters = dict()
+    if args.record is not None:
+       Parameters.update({'record':args.record})
+    if args.file is not None:
+        Parameters.update({'file':args.file})
+            
     if args.ausername:
         authUsername = args.ausername
     if args.apassword:
@@ -48,9 +45,9 @@ def main(argv):
 
     # KEEPER COMMAND
     command = RecordUploadAttachmentCommand()
-    recordResult = command.execute(my_params, record=recordUID, file=recordFile)
+    result = command.execute(my_params, **Parameters)
     print("Successfully")
-    return recordResult
+    return result
 
 if __name__ == "__main__":
     main(sys.argv[1:])
