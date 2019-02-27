2b208da1083d0578f35f901d09ac7e2b39f4032f
tonylanglet <tony.langlet@core-portal.com>
2019-02-27 23:46:36 +0100

    Update Get-KeeperRecordAttachment.py

--- PyScripts/Get-KeeperRecordAttachment.py
+++ PyScripts/Get-KeeperRecordAttachment.py
@@ -2,7 +2,6 @@ import sys
 import getopt
 import getpass
 import string
-import re
 import argparse
 
 from keepercommander.record import Record
@@ -14,21 +13,20 @@ my_params = KeeperParams()
       
 # MAIN FUNCTION
 def main(argv):
-    # Variables
-    recordUID = None
     # Authentication credentials
     authUsername = None
     authPassword = None
 
     # Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--record", type=str, help="Folder UID", required=True)
-    parser.add_argument("-auser", "--ausername", type=str, help="Authentication username", required=True)
-    parser.add_argument("-apass", "--apassword", type=str, help="Authentication password", required=True)
+    parser.add_argument('-r', '--record', type=str, help='Folder UID', required=True)
+    parser.add_argument('-auser', '--ausername', type=str, help='Authentication username', required=True)
+    parser.add_argument('-apass', '--apassword', type=str, help='Authentication password', required=True)
     args = parser.parse_args()
 
-    if args.record:
-        recordUID = args.record
+    if args.record is not None:
+        Parameters.update({'record':args.record})
+      
     if args.ausername:
         authUsername = args.ausername
     if args.apassword:
@@ -43,7 +41,7 @@ def main(argv):
 
     # KEEPER COMMAND
     command = RecordDownloadAttachmentCommand()
-    result = command.execute(my_params, record=recordUID)
+    result = command.execute(my_params, **Parameters)
     print("Successfully")
     return result
 
