8c73b92b1a0d6e34e8b3a90d77ad0f90583a4250
tonylanglet <tony.langlet@core-portal.com>
2019-03-01 22:41:32 +0100

    Update Get-KeeperRecord.py

--- PyScripts/Get-KeeperRecord.py
+++ PyScripts/Get-KeeperRecord.py
@@ -46,7 +46,6 @@ def main(argv):
     # KEEPER COMMAND
     command = RecordGetUidCommand()
     result = command.execute(my_params, **Parameters)
-    print("Successfully fetched record")
     return result
 
 if __name__ == "__main__":
