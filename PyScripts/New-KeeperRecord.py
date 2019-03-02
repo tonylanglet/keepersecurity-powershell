5fbbc4d4a568365ae0ed7e6eb393471d1a7cf260
tonylanglet <tony.langlet@core-portal.com>
2019-03-02 01:36:46 +0100

    Update New-KeeperRecord.py

--- PyScripts/New-KeeperRecord.py
+++ PyScripts/New-KeeperRecord.py
@@ -62,7 +62,6 @@ def main(argv):
 	api.sync_down(my_params)
 
     # KEEPER COMMAND 
-	print(Parameters)
 	command = RecordAddCommand()
 	record_uid = command.execute(my_params, **Parameters)
 	return record_uid
