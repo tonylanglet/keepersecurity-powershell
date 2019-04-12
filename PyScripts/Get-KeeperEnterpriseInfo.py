99522d34ee11772de7509eef62e59cbfc6835096
tonylanglet <tony.langlet@core-portal.com>
2019-04-12 22:09:04 +0200

    Update Get-KeeperEnterpriseInfo.py

--- PyScripts/Get-KeeperEnterpriseInfo.py
+++ PyScripts/Get-KeeperEnterpriseInfo.py
@@ -20,9 +20,9 @@ def main(argv):
     # Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--nodes', dest='nodes', type=bool, help='print node tree')
-    parser.add_argument('--users', dest='users', type=bool', help='print user list')
-    parser.add_argument('--teams', dest='teams', type=bool', help='print team list')
-    parser.add_argument('--roles', dest='roles', type=bool', help='print role list')
+    parser.add_argument('--users', dest='users', type=bool, help='print user list')
+    parser.add_argument('--teams', dest='teams', type=bool, help='print team list')
+    parser.add_argument('--roles', dest='roles', type=bool, help='print role list')
     parser.add_argument('--node', dest='node', action='store', help='limit results to node (name or ID)')
     parser.add_argument('-auser', '--ausername', type=str, help='Authentication username', required=True)
     parser.add_argument('-apass', '--apassword', type=str, help='Authentication password', required=True)
@@ -30,15 +30,15 @@ def main(argv):
 
     Parameters = dict()
     if args.nodes is not None:
-        Parameters.update({'nodes':args.nodes)}
+        Parameters.update({'nodes':args.nodes})
     if args.users is not None:
-        Parameters.update({'users':args.users)}
+        Parameters.update({'users':args.users})
     if args.roles is not None:
-        Parameters.update({'roles':args.roles)}
+        Parameters.update({'roles':args.roles})
     if args.teams is not None:
-        Parameters.update({'teams':args.teams)}
+        Parameters.update({'teams':args.teams})
     if args.node is not None:
-       Parameters.update({'node':args.node)}
+       Parameters.update({'node':args.node})
                           
     if args.ausername:
         authUsername = args.ausername
